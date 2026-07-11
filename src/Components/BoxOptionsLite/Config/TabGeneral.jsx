28f452bb4a49522a3e09e93ba98bb289ef5514c7
diegobckn <diego.bckn@gmail.com>
2026-07-11 00:48:59 -0300

    aut crear prod

--- src/Components/BoxOptionsLite/Config/TabGeneral.jsx
+++ src/Components/BoxOptionsLite/Config/TabGeneral.jsx
@@ -389,7 +389,7 @@ const TabGeneral = ({
         />
       </Grid>
       <Grid item xs={12} md={12} lg={12}>
-        <InputCheckbox
+        <InputCheckboxAutorizar
           inputState={[crearProductoNoEncontrado, setCrearProductoNoEncontrado]}
           label={"Crear Producto No Encontrado"}
         />
